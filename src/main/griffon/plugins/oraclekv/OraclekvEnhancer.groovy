9fd6fa1a695a56db357288002e3e2cb9e5774ee5
aalmiray <aalmiray@gmail.com>
2012-02-06 11:59:10 +0100

    Extract Provider interface; Added enhancer

---
+++ src/main/griffon/plugins/oraclekv/OraclekvEnhancer.groovy
@@ -0,0 +1,43 @@
+/*
+    griffon-oraclekv plugin
+    Copyright (C) 2012 Andres Almiray
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package griffon.plugins.oraclekv
+
+import griffon.util.CallableWithArgs
+
+/**
+ * @author Andres Almiray
+ */
+final class OraclekvEnhancer {
+    private OraclekvEnhancer() {}
+    
+    static void enhance(MetaClass mc, OraclekvProvider provider = OraclekvStoreHolder.instance) {
+        mc.withOraclekv = {Closure closure ->
+            provider.withOraclekv('default', closure)
+        }
+        mc.withOraclekv << {String storeName, Closure closure ->
+            provider.withOraclekv(storeName, closure)
+        }
+        mc.withOraclekv << {CallableWithArgs callable ->
+            provider.withOraclekv('default', callable)
+        }
+        mc.withOraclekv << {String storeName, CallableWithArgs callable ->
+            provider.withOraclekv(storeName, callable)
+        }
+    }
+}
